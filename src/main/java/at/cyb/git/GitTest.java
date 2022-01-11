4ab37d2e37d037f0d7d8d72979f41467e8af05f2
cyb <785707541@qq.com>
2022-01-11 13:49:43 +0800

    first commit

---
+++ src/main/java/at/cyb/git/GitTest.java
@@ -0,0 +1,11 @@
+package at.cyb.git;
+
+/**
+ * @author cyb
+ * @create 2022-01-11 12:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
